f32765ec074b4a88a077772255b96571085c4d52
Andrew Duong <andrewduong77@yahoo.com>
2018-02-13 07:49:39 -0600

    more progress on menu part 1

--- Homework/Homework 4/HW4/hw4/main.cpp
+++ Homework/Homework 4/HW4/hw4/main.cpp
@@ -27,11 +27,11 @@ int main()
     cout << "3: Delete a transaction(s)" << endl;
     cout << "4: Average transaction value" << endl;
     cout << "5: Bonus winner" << endl;
-    cout << "? |" << endl;
+    cout << "6: Exit" << endl;
     Transaction_List transactions {};
     int select;
     cin >> select;
-    while(select < 1 && select > 5)
+    while(select != 6)
     {
         switch(select)
         {
@@ -64,6 +64,54 @@ int main()
                 cout << "transaction added" << endl;
             if(!transactions.add_transaction(date,transaction))
                 cout << "transaction not added" << endl;
+            break;
+        case 3:
+            cout << "What would you like to delete by?" << endl;
+            cout << "1 Date" << endl;
+            cout << "2 Name" << endl;
+            int select1;
+            cin >> select1;
+            while(select1 < 1 && select1 > 2)
+            {
+                cout << "Invalid selection. Try Again: " << endl;
+                cout << "What would you like to delete by?" << endl;
+                cout << "1 Date" >> endl;
+                cout << "2 Name" >> endl;
+                cin >> select1;
+            }
+            if(select1 == 1)
+            {
+                cout << "Please enter a date to be deleted: ";
+                int year1, month1, day1, hour1, min1, second1;
+                cin >> year1;
+                cin >> month1;
+                cin >> day1;
+                cin >> hour1;
+                cin >> min1;
+                cin >> second1;
+                Date date1 {year1, month1, day1, hour1, min1, second1};
+                transactions.delete_transaction_by_date(date1)
+            }
+            else
+            {
+                cout << "Please enter the name of the transaction(s) to be deleted: ";
+                string name1;
+                cin >> name1;
+                transactions.delete_transactions_by_name(name1);
+            }
+            break;
+        case 4:
+            double avg = transactions.get_average_transaction();
+            cout << "The average transaction is " + avg + "." << endl;
+            break;
+        case 5:
+            string the_bonus = transactions.bonus();
+            cout << "The bonus winner is " + the_bonus + "." << endl;
+            break;
+        case 6:
+            break;
+        default:
+            cout << "Invalid selection. Try again!" << endl;
         }
     }
 }
