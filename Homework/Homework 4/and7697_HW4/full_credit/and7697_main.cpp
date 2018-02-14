357df950cc688e33f7e606e72159a79fb802ca9c
Andrew Duong <andrewduong77@yahoo.com>
2018-02-13 19:02:22 -0600

    deleted comments

--- Homework/Homework 4/and7697_HW4/full_credit/and7697_main.cpp
+++ Homework/Homework 4/and7697_HW4/full_credit/and7697_main.cpp
@@ -99,18 +99,3 @@ int main()
         }
     }while(select != 6);
 }
-
-//#include "and7697_Transaction_List.h"
-//
-//int main()
-//{
-//    Transaction_List transactions {};
-//    Date d1 {2014,10,28,16,30,0};
-//    Transaction t1 {439.87,"iPhone_5s"};
-//    cout << a << end;
-//    if(transactions.add_transaction(d1, t1))
-//        cout << "Event added" << endl;
-//    if(!transactions.add_transaction(d1, t1))
-//        cout << "Event not added" << endl;
-//    return 0;
-//}
