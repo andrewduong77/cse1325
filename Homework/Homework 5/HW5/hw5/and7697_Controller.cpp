ff356068a5077bdf4dd9cee665ed8eb0e37a622a
andrewduong77 <andrewduong77@yahoo.com>
2018-02-24 18:04:39 -0600

    more progress on controller

--- Homework/Homework 5/HW5/hw5/and7697_Controller.cpp
+++ Homework/Homework 5/HW5/hw5/and7697_Controller.cpp
@@ -14,10 +14,6 @@ void Controller::cli()
 
 void Controller::execute_cmd(int cmd)
 {
-    int year, month, day, hour, minute, second;
-    Date date {0, 0, 0, 0, 0, 0};
-    double price;
-    string name;
     Transaction transaction {0, ""};
     double avg;
     string the_bonus;
@@ -27,42 +23,10 @@ void Controller::execute_cmd(int cmd)
         cout << view.print_all_transactions();
         break;
     case 2:
-        cout << view.prompt_for_date();
-        cin >> year >> month >> day >> hour >> minute >> second;
-        date = {year, month, day, hour, minute, second};
-        cout << view.prompt_for_price();
-        cin >> price;
-        cout << view.prompt_for_name();
-        cin >> name;
-        transaction = {price, name};
-        if(transactions.add_transaction(date,transaction))
-            cout << "transaction added" << endl;
-        if(!transactions.add_transaction(date,transaction))
-            cout << "transaction not added" << endl;
+        add_transaction();
         break;
     case 3:
-        cout << view.get_delete_transaction_menu();
-        int cmd1;
-        cin >> cmd1;
-        while(cmd1 < 1 || cmd1 > 3)
-        {
-            cout << view.print_try_again_message();
-            cout << view.get_delete_transaction_menu();
-            cin >> cmd1;
-        }
-        if(cmd1 == 1)
-        {
-            cout << "Please enter a date to be deleted: ";
-            cin >> year >> month >> day >> hour >> minute >> second;
-            date = {year, month, day, hour, minute, second};
-            transactions.delete_transaction_by_date(date);
-        }
-        else if(cmd1 == 2)
-        {
-            cout << "Please enter the name of the transaction(s) to be deleted: ";
-            cin >> name;
-            transactions.delete_transactions_by_name(name);
-        }
+        delete_transaction();
         break;
     case 4:
         avg = transactions.get_average_transaction();
@@ -81,7 +45,52 @@ void Controller::execute_cmd(int cmd)
     }
 }
 
+void Controller::add_transaction()
+{
+    Date date {0, 0, 0, 0, 0, 0};
+    Transaction transaction {0, ""};
+    int year, month, day, hour, minute, second;
+    double price;
+    string name;
+    cout << view.prompt_for_date();
+    cin >> year >> month >> day >> hour >> minute >> second;
+    date = {year, month, day, hour, minute, second};
+    cout << view.prompt_for_price();
+    cin >> price;
+    cout << view.prompt_for_name();
+    cin >> name;
+    transaction = {price, name};
+    if(transactions.add_transaction(date,transaction))
+        cout << "transaction added" << endl;
+    if(!transactions.add_transaction(date,transaction))
+        cout << "transaction not added" << endl;
+}
+
 void Controller::delete_transaction()
 {
-
+    Date date {0, 0, 0, 0, 0, 0};
+    int year, month, day, hour, minute, second;
+    string name;
+    cout << view.get_delete_transaction_menu();
+    int cmd1;
+    cin >> cmd1;
+    while(cmd1 < 1 || cmd1 > 3)
+    {
+        cout << view.print_try_again_message();
+        cout << view.get_delete_transaction_menu();
+        cin >> cmd1;
+    }
+    if(cmd1 == 1)
+    {
+        cout << "Please enter a date to be deleted: ";
+        cin >> year >> month >> day >> hour >> minute >> second;
+        date = {year, month, day, hour, minute, second};
+        transactions.delete_transaction_by_date(date);
+    }
+    else if(cmd1 == 2)
+    {
+        cout << "Please enter the name of the transaction(s) to be deleted: ";
+        cin >> name;
+        transactions.delete_transactions_by_name(name);
+    }
 }
--- Homework/Homework 5/HW5/hw5/and7697_Controller.h
+++ Homework/Homework 5/HW5/hw5/and7697_Controller.h
@@ -6,6 +6,7 @@ public:
     Controller(Transaction_List& t, View& v) : transactions(t), view(v) {};
     void cli();
     void execute_cmd(int cmd);
+    void add_transaction();
     void delete_transaction();
 private:
     Transaction_List& transactions;
