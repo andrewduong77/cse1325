1b5386201c7f8401ac6678d591ac85f60578ca0c
andrewduong77 <andrewduong77@yahoo.com>
2018-02-25 22:36:46 -0600

    completed bonus and made bonus_1 folder for submission

---
+++ Homework/Homework 5/and7697_HW5/bonus_1/and7697_Transaction.cpp
@@ -0,0 +1,22 @@
+#include "and7697_Transaction.h"
+
+double Transaction::get_price()
+{
+    return price;
+}
+
+string Transaction::get_name()
+{
+    return name;
+}
+
+string Transaction::to_string() const
+{
+    return std::to_string(price) + " " + name;
+}
+
+ostream& operator<<(ostream& ost, const Transaction& trans_two)
+{
+    ost << trans_two.to_string();
+    return ost;
+}
---
+++ Homework/Homework 5/and7697_HW5/bonus_1/and7697_View.cpp
@@ -0,0 +1,55 @@
+#include "and7697_View.h"
+
+string View::get_menu()
+{
+    return R"(
+=============================
+Transaction Management System
+=============================
+
+(1) Load transactions to file
+(2) Save transactions to file
+(3) Print all transactions
+(4) Add a transaction
+(5) Delete a transaction(s)
+(6) Average transaction
+(7) Bonus winner
+(8) Delete all transactions
+(9) Exit
+_> )";
+}
+
+string View::print_all_transactions()
+{
+    return transactions.to_string();
+}
+
+string View::prompt_for_date()
+{
+    return "Please enter a date: ";
+}
+
+string View::prompt_for_name()
+{
+    return "Please enter a name: ";
+}
+
+string View::prompt_for_price()
+{
+    return "Please enter a price: ";
+}
+
+string View::get_delete_transaction_menu()
+{
+    return R"(
+What would you like to delete by?
+(1) Date
+(2) Name
+(3) Cancel
+_> )";
+}
+
+string View::print_try_again_message()
+{
+    return "Invalid selection. Try again!";
+}
