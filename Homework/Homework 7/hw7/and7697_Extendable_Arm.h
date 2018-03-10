56e1d8986c6e2c997a2380b13288663b9ff26e12
andrewduong77 <andrewduong77@yahoo.com>
2018-03-10 16:26:39 -0600

    fixed error in and7697_Extendable_Arm.h, delete duplicated extend() and retract()

--- Homework/Homework 7/hw7/and7697_Extendable_Arm.h
+++ Homework/Homework 7/hw7/and7697_Extendable_Arm.h
@@ -10,8 +10,6 @@ class Extendable_Arm : public Arm_Robot
 public:
     Extendable_Arm(int mn, string n, int bl, int l, int wl, int el) : Arm_Robot(mn, n, bl, l, wl), Robot(mn, n, bl), extend_length {el}, is_extended {false} {};
     bool move(int x, int y);
-    bool extend();
-    bool retract();
 protected:
     int extend_length;
     bool is_extended;
