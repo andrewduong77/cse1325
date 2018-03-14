8ed7a868e234f826f2e2857b39498b8d346fdf00
Andrew Duong <andrewduong77@yahoo.com>
2018-03-13 20:09:53 -0500

    completed and7697_Mobile_Arm.cpp, compiled and ran successfully

--- Homework/Homework 7/hw7/and7697_Mobile_Arm.cpp
+++ Homework/Homework 7/hw7/and7697_Mobile_Arm.cpp
@@ -2,25 +2,31 @@
 
 bool Mobile_Arm::move(int x, int y)
 {
-
+    return Mobile_Robot::move(x, y);
 }
 
 bool Mobile_Arm::move(int x, int y, int speed)
 {
-
+    return Mobile_Robot::move(x, y, speed);
 }
 
 bool Mobile_Arm::move_arm(int x, int y)
 {
-
+    return Super_Arm::move(x, y);
 }
 
 bool Mobile_Arm::move(int base_x, int base_y, int arm_x, int arm_y)
 {
-
+    bool base, arm;
+    base = Mobile_Robot::move(base_x, base_y);
+    arm = Super_Arm::move(arm_x, arm_y);
+    return base && arm;
 }
 
 bool Mobile_Arm::move(int base_x, int base_y, int arm_x, int arm_y, int speed)
 {
-
+    bool base, arm;
+    base = Mobile_Robot::move(base_x, base_y);
+    arm = Super_Arm::move(arm_x, arm_y);
+    return base && arm;
 }
