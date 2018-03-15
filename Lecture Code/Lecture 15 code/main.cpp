ecbd12b7b92a0bbd4ca025b07b71833ca57bb0fe
Andrew Duong <andrewduong77@yahoo.com>
2018-03-14 21:34:18 -0500

    fixed file naming errors and makefile for lecture code 15

--- Lecture Code/Lecture 15 code/main.cpp
+++ Lecture Code/Lecture 15 code/main.cpp
@@ -2,7 +2,7 @@
 //#include "Sales.h"
 //#include "Manager.h"
 #include "SalesManager.h"
-#include "Cleark.h"
+#include "Clerk.h"
 #include <iostream>
 #include <vector>
 
@@ -13,12 +13,12 @@ int main()
     cout << e->pay(1000) << endl;
     ((Stock_boy* )e)->add_hours(41);
     cout << e->pay(1000) << endl;
-    
+
     vector<Employee*> staff;
     staff.push_back(e);
     //staff.push_back(new Clerk("Clark", 790, 8.25));
     staff.push_back(new Sales("Sales", 791, 0.02));
-    
+
     for(Employee* it : staff)
     {
         if(it->get_type() == "Stock_Boy")
@@ -29,36 +29,36 @@ int main()
         cout << it->get_name() << " " << it->pay(0) << endl;
         //cout<< it->get_type();
     }
-    
+
     vector<Stock_boy> numbers;
    // Stock_boy temp {variables};
     //numbers.push_back(temp);
     numbers.emplace_back("q", 456, 456);
-    
+
     /*//Employee jeff {"Jeff", 1234567890};
-    
+
     //cout << jeff.pay(100) << endl;
-    
+
     Stock_boy squirtle {"Squirtle", 987654321, 10};
     squirtle.add_hours(40);
     cout << squirtle.get_name() << endl;
     cout << squirtle.pay(0) << endl;
-    
-    
+
+
     Sales saley_boi {"Saley_boi", 1337, 0.01};
     saley_boi.add_sales(200000.00);
     cout << saley_boi.pay(20) << endl;
-    
+
     Manager CEO {"Jeff Bezos", 741852963, 81840.00};
     cout << CEO.pay(1600000) << endl;
-    
+
     cout << squirtle.pay(1000) << endl;
-    
+
     SalesManager bob {"Bob", 8541987, .02, 300};
     cout << bob.pay(0) << endl;
     bob.add_sales(1000000);
     cout << bob.pay(65) << endl;
-    
+
     Clerk clark {"Clark Kent", 894168598, 12};
     clark.add_hours(4);
     cout << clark.pay(52) << endl;*/
