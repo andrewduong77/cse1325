74dca7c224b2826c3c2cbc1502b6b6eb654aa2bc
Andrew Duong <andrewduong77@yahoo.com>
2018-03-20 03:58:42 -0500

    included extended arm conditions into and7697_Super_Arm.cpp and fixed ambiguous errors with Extendable_Arm::length

--- Homework/Homework 7/hw7/and7697_Super_Arm.cpp
+++ Homework/Homework 7/hw7/and7697_Super_Arm.cpp
@@ -6,6 +6,21 @@ bool Super_Arm::move(int x, int y)
     int d_y = abs(y - position.second);
     double d_p = calculate_distance(d_x, d_y); // d_p is distance from position
     double d_o = calculate_distance(abs(x), abs(y)); // d_o is distance from origin (0,0)
+    if(d_o > Extendable_Arm::length)
+    {
+        if(d_o > Extendable_Arm::length + extend_length)
+        {
+            cout << "Arm length is still not long enough." << endl;
+            return false;
+        }
+        else if(extend())
+            cout << "Arm extended." << endl;
+    }
+    else
+    {
+        if(retract())
+            cout << "Arm retracted." << endl;
+    }
     int load = 1;
     if(Extendable_Arm::is_holding || Powered_Arm::is_holding)
         load++;
