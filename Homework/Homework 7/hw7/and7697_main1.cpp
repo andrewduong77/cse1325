aa5489fef260e7a0cda5c3fc1cc135f0c6a7ac2d
Andrew Duong <andrewduong77@yahoo.com>
2018-03-20 04:34:41 -0500

    more progress on and7697_main1.cpp, update filenames to meet deliverables requirement

--- Homework/Homework 7/hw7/and7697_main1.cpp
+++ Homework/Homework 7/hw7/and7697_main1.cpp
@@ -120,6 +120,39 @@ int main()
         cout << "Fanny was unable to drop an object of a weight of " << defaultfloat << weight << "." << endl;
 
     cout << "Fanny's battery level is currently at " << fixed << setprecision(0) << fanny.get_battery_percentage() << "%." << endl;
+
+    x = -3;
+    y = 2;
+    if(fanny.move(x, y))
+        cout << "Fanny was able to move to (" << defaultfloat << x << "," << y << ")." << endl;
+    else
+        cout << "Fanny was unable to move to (" << defaultfloat << x << "," << y << ")." << endl;
+
+    cout << "Fanny's battery level is currently at " << fixed << setprecision(0) << fanny.get_battery_percentage() << "%." << endl;
+
+    weight = 9;
+    if(fanny.Powered_Arm::pick_up(weight))
+        cout << "Fanny was able to pick up an object of a weight of " << defaultfloat << weight << "." << endl;
+    else
+        cout << "Fanny was unable to pick up an object of a weight of " << defaultfloat << weight << "." << endl;
+
+    cout << "Fanny's battery level is currently at " << fixed << setprecision(0) << fanny.get_battery_percentage() << "%." << endl;
+
+    x = 8;
+    y = -4;
+    if(fanny.move(x, y))
+        cout << "Fanny was able to move to (" << defaultfloat << x << "," << y << ")." << endl;
+    else
+        cout << "Fanny was unable to move to (" << defaultfloat << x << "," << y << ")." << endl;
+
+    cout << "Fanny's battery level is currently at " << fixed << setprecision(0) << fanny.get_battery_percentage() << "%." << endl;
+
+    if(fanny.Powered_Arm::drop())
+        cout << "Fanny was able to drop an object of a weight of " << defaultfloat << weight << "." << endl;
+    else
+        cout << "Fanny was unable to drop an object of a weight of " << defaultfloat << weight << "." << endl;
+
+    cout << "Fanny's battery level is currently at " << fixed << setprecision(0) << fanny.get_battery_percentage() << "%." << endl;
 }
 
 //int main()
