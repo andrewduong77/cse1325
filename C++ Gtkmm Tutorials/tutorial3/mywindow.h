66a0231cabee9c8e100161bf99c5d817b1fb5f14
Andrew Duong <andrewduong77@yahoo.com>
2018-04-17 22:24:21 -0500

    added label to main menu, added C++ Gtkmm Tutorials files

---
+++ C++ Gtkmm Tutorials/tutorial3/mywindow.h
@@ -0,0 +1,20 @@
+#ifndef MYWINDOW_H_INCLUDED
+#define MYWINDOW_H_INCLUDED
+
+#include <gtkmm.h>
+
+class mywindow : public Gtk::Window
+{
+    public:
+        mywindow();
+        virtual ~mywindow();
+    protected:
+        void on_big_button1_click();
+        void on_button2_click();
+        void on_button3_click();
+        void on_quit_click();
+        void dialog(Glib::ustring msg);
+    private:
+};
+
+#endif // MYWINDOW_H_INCLUDED
